e017c54ef48cec43bf38e8c9def1d683fd57b2ce
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-03-04 16:56:00 +0100

    HtmlHelperExtensions update and formating

--- src/Geta.EPi.Extensions/HttpContextBaseExtensions.cs
+++ src/Geta.EPi.Extensions/HttpContextBaseExtensions.cs
@@ -1,19 +1,19 @@
-﻿using System.Web;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace Geta.EPi.Extensions
 {
     /// <summary>
-    /// Extensions for <see cref="HttpContextBase"/>
+    /// Extensions for <see cref="HttpContext"/>
     /// </summary>
     public static class HttpContextBaseExtensions
     {
         /// <summary>
-        ///     Checks HttpContextBase.Items for a key named IsBlockPreviewTemplate and if it's set to true. 
+        ///     Checks HttpContextBase.Items for a key named IsBlockPreviewTemplate and if it's set to true.
         ///     This key is set through the TemplateResolver.TemplateResolved event in the <see cref="ExtensionsInitializationModule">initialization module</see>
         /// </summary>
         /// <param name="httpContext">The current HttpContextBase instance</param>
         /// <returns>True/false</returns>
-        public static bool IsBlockPreview(this HttpContextBase httpContext)
+        public static bool IsBlockPreview(this HttpContext httpContext)
         {
             object isBlockPreview = httpContext?.Items["IsBlockPreviewTemplate"];
 
