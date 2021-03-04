748f88ec02b9e0ba3f103800dbf9a8d46ba39734
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-03-04 16:55:24 +0100

    Add internal StringHelpers with AppendTrailingSlash method

---
+++ src/Geta.EPi.Extensions/Helpers/StringHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace Geta.EPi.Extensions.Helpers
+{
+    internal static class StringHelpers
+    {
+        internal static string AppendTrailingSlash(this string path)
+        {
+            if (path == null)
+                return null;
+            var length = path.Length;
+            if (length == 0 || path[length - 1] == '/')
+                return path;
+            path += "/";
+            return path;
+        }
+    }
+}
