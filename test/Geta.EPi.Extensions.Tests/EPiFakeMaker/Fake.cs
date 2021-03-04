f5840f5b0e4720e051d3671a57e04c1c9ec8d00c
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-03-04 17:09:34 +0100

    Add EpiFakeMaker source code files

---
+++ test/Geta.EPi.Extensions.Tests/EPiFakeMaker/Fake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using EPiServer.Core;
+
+[assembly: InternalsVisibleTo("FakeMaker.Commerce")]
+
+namespace Geta.EPi.Extensions.Tests.EPiFakeMaker
+{
+    public abstract class Fake : IFake
+    {
+        public abstract IList<IFake> Children { get; }
+        public abstract IContent Content { get; protected set; }
+
+        internal abstract void HelpCreatingMockForCurrentType(IFakeMaker maker);
+    }
+}
---
+++ test/Geta.EPi.Extensions.Tests/EPiFakeMaker/IFake.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using EPiServer.Core;
+
+namespace Geta.EPi.Extensions.Tests.EPiFakeMaker
+{
+    public interface IFake
+    {
+        IContent Content { get; }
+        IList<IFake> Children { get; }
+    }
+}
