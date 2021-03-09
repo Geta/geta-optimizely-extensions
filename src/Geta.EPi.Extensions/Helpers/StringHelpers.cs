f0ee379a0b232146b252e10060fc54bd7fbff14d
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2021-03-09 09:04:41 +0100

    fix: AppendTrailingSlash refactor

--- src/Geta.EPi.Extensions/Helpers/StringHelpers.cs
+++ src/Geta.EPi.Extensions/Helpers/StringHelpers.cs
@@ -1,13 +1,12 @@
-﻿namespace Geta.EPi.Extensions.Helpers
+﻿using Geta.Net.Extensions;
+
+namespace Geta.EPi.Extensions.Helpers
 {
     internal static class StringHelpers
     {
         internal static string AppendTrailingSlash(this string path)
         {
-            if (path == null)
-                return null;
-            var length = path.Length;
-            if (length == 0 || path[length - 1] == '/')
+            if (path.IsNullOrEmpty() || path.EndsWith('/'))
                 return path;
             path += "/";
             return path;
