6bac5d855e5bbbe2ac14047b73ff87dd8569a87f
Jevgenij Puchko <jevgen.pucko@gmail.com>
2025-02-10 14:39:58 +0200

    use aspire project

---
+++ src/Geta.Optimizely.Extensions.Web/Program.cs
@@ -0,0 +1,13 @@
+﻿using Geta.Optimizely.Extensions.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureCmsDefaults()
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStartup<Startup>();
+        webBuilder.UseContentRoot(Path.GetFullPath("../../sandbox/geta-packages-foundation-sandbox/src/Foundation"));
+    })
+    .Build()
+    .Run();
